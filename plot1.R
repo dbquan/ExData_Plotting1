dcf0c8b2dd2eb1cd2d5683685a28045581fdaec2
Derrick <dbquan88@gmail.com>
2015-08-09 16:14:24 -0700

    add

---
+++ plot1.R
@@ -0,0 +1,8 @@
+setwd("/Users/Derrick/Desktop/git/datasciencecoursera")
+
+powerdata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", numerals = "allow.loss")
+
+subpower <- subset(powerdata, powerdata$Date == "2/2/2007" | powerdata$Date == "1/2/2007")
+
+hist(subpower$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
