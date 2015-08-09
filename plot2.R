8126845ad7844754586a71f4f3dfc60759ef1625
Derrick <dbquan88@gmail.com>
2015-08-09 16:29:04 -0700

    addmore

--- plot2.R
+++ plot2.R
@@ -3,5 +3,6 @@ setwd("/Users/Derrick/Desktop/git/datasciencecoursera")
 powerdata <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", numerals = "allow.loss")
 
 subpower <- subset(powerdata, powerdata$Date == "2/2/2007" | powerdata$Date == "1/2/2007")
-
+png("plot2.png", width=480, height=480)
 plot(subpower$Global_active_power, type = "l", ylab ="Global Active Power (kilowatts)")
+dev.off()
